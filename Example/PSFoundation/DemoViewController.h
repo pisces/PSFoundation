6f7085b840b0016b075f694f5e30152aee8e19c6
pisces <hh963103@gmail.com>
2016-04-28 15:36:55 +0900

    Initial commit

---
+++ Example/PSFoundation/DemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  DemoViewController.h
+//  PSFoundation
+//
+//  Created by pisces on 04/28/2016.
+//  Copyright (c) 2016 pisces. All rights reserved.
+//
+
+@import UIKit;
+
+@interface DemoViewController : UIViewController
+
+@end
---
+++ Example/PSFoundation/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PSFoundation
+//
+//  Created by pisces on 04/28/2016.
+//  Copyright (c) 2016 pisces. All rights reserved.
+//
+
+@import UIKit;
+#import "DemoAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DemoAppDelegate class]));
+    }
+}
